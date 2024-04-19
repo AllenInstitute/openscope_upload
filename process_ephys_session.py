ce19041cf3739f15c4dd3a388fcf2564abc5822d
Carter Peene <carter.peene@alleninstitute.org>
2024-04-19 14:57:07 -0700

    Added stim software and video and behavior data streams

--- process_ephys_session.py
+++ process_ephys_session.py
@@ -4,17 +4,19 @@ import argparse
 import datetime
 import io
 import json
-import np_session
-import np_tools
 import npc_ephys
+import npc_mvr
+import np_session
 import npc_session
 import npc_sessions
 import npc_sync
+import numpy as np
 import pandas as pd
 import re
 from aind_data_schema.models.modalities import Modality as schema_modalities
 from aind_data_schema.models.coordinates import Coordinates3d as schema_coordinates
 from utils import process_ephys_sync as stim_utils
+from utils import pickle_functions as pkl_utils
 
 
 # def valid_probe_folder(probe_letter, recording_dir):
@@ -76,7 +78,7 @@ def sync_data(sync_path):
     return npc_sync.SyncDataset(io.BytesIO(sync_path.read_bytes()))
 
 
-def session_start_end_times(sync_path) -> tuple[int, int]:    
+def session_start_end_times(sync_path):    
     start_timestamp = sync_data(sync_path).start_time
     end_timestamp = sync_data(sync_path).stop_time
     return (start_timestamp, end_timestamp)
@@ -93,7 +95,7 @@ def iacuc_protocol() -> str:
 def ephys_stream(sync_path, recording_dirs, motor_locs_path, platform_json) -> session_schema.Stream:
     available_probes = get_available_probes(platform_json)
 
-    start_time, end_time = session_start_end_times(sync_path)
+    start_time, _ = session_start_end_times(sync_path)
     epmods = ephys_modules(available_probes, motor_locs_path)
 
     times = npc_ephys.get_ephys_timing_on_sync(sync=sync_path, recording_dirs=recording_dirs)
@@ -114,9 +116,35 @@ def ephys_stream(sync_path, recording_dirs, motor_locs_path, platform_json) -> s
     )
 
 
-def data_streams() -> tuple[session_schema.Stream, ...]:
+def sync_stream(sync_path):
+    sync_start, sync_end = session_start_end_times(sync_path)
+    return session_schema.Stream(
+            stream_start_time=sync_start,
+            stream_end_time=sync_end,
+            stream_modalities=[schema_modalities.BEHAVIOR],
+            daq_names=["Sync"]
+    )
+
+def video_stream(npexp_path, sync_path):
+    session_start_time, _ = session_start_end_times(sync_path)
+    video_frame_times = npc_mvr.mvr.get_video_frame_times(sync_path, npexp_path)
+
+    stream_first_time = min(np.nanmin(timestamps) for timestamps in video_frame_times.values())
+    stream_last_time = max(np.nanmax(timestamps) for timestamps in video_frame_times.values())
+
+    return session_schema.Stream(
+        stream_start_time=session_start_time + datetime.timedelta(seconds=stream_first_time),
+        stream_end_time=session_start_time + datetime.timedelta(seconds=stream_last_time),
+        camera_names=["Front camera", "Side camera", "Eye camera"],
+        stream_modalities=[schema_modalities.BEHAVIOR_VIDEOS],
+    )
+
+
+def data_streams(npexp_path, sync_path, recording_dirs, motor_locs_path, platform_json) -> tuple[session_schema.Stream, ...]:
     data_streams = []
-    data_streams.append(ephys_stream())
+    data_streams.append(ephys_stream(sync_path, recording_dirs, motor_locs_path, platform_json))
+    data_streams.append(sync_stream(sync_path))
+    data_streams.append(video_stream(npexp_path, sync_path))
     return tuple(data_streams)
 
 
@@ -131,12 +159,23 @@ def active_mouse_platform() -> str:
 def extract_epochs(stim_table):
     epochs = []
 
-    current_epoch = [None, 0.0, 0.0]
+    current_epoch = [None, 0.0, 0.0, {}, set()]
+    epoch_start_idx = 0
     for i, row in stim_table.iterrows():
         if row["stimulus_name"] != current_epoch[0]:
-            # end current epoch, start a new epoch
+            # end current epoch, summarize this epochs params and stim templates and start a new epoch
+            for column in stim_table:
+                if column in ("Start", "End", "stimulus_name"):
+                    continue
+                param_set = set(stim_table[column][epoch_start_idx:i].dropna())
+                if column == "stim_template":
+                    current_epoch[4] = param_set
+                else:
+                    current_epoch[3][column] = param_set
+
             epochs.append(current_epoch)
-            current_epoch = [row["stimulus_name"], row["Start"], row["End"]]
+            epoch_start_idx = i
+            current_epoch = [row["stimulus_name"], row["Start"], row["End"], {}, set()]
         else:
             # otherwise, keep pushing epoch end time later
             current_epoch[2] = row["End"]
@@ -145,28 +184,31 @@ def extract_epochs(stim_table):
     return epochs[1:]
 
 
-def stim_epochs_from_table(stim_table_path, sync_path):
+def stim_epochs_from_table(session, pkl_path, stim_table_path, sync_path):
     stim = aind_data_schema.core.session.StimulusModality
 
     stim_table = pd.read_csv(stim_table_path)
     session_start_time, _ = session_start_end_times(sync_path)
-    print(stim_table)
 
-    epochs = extract_epochs(stim_table)
     schema_epochs = []
-    for epoch_name, epoch_start, epoch_end in epochs:
+    for epoch_name, epoch_start, epoch_end, stim_params, stim_template_names in extract_epochs(stim_table):
         schema_epochs.append(
             session_schema.StimulusEpoch(
                 stimulus_start_time=session_start_time + datetime.timedelta(seconds=epoch_start),
                 stimulus_end_time=session_start_time + datetime.timedelta(seconds=epoch_end),
                 stimulus_name=epoch_name,
-                # script=aind_data_schema.models.devices.Software(
-                #         name='',
-                #         version=,
-                #         url=,
-                #     ),
+                software=[aind_data_schema.models.devices.Software(
+                        name="camstim",
+                        version=pkl_utils.load_pkl(pkl_path)['platform']['camstim'].split('+')[0],
+                        url='https://eng-gitlab.corp.alleninstitute.org/braintv/camstim'
+                    )],
+                script=aind_data_schema.models.devices.Software(
+                        name=session.mtrain['regimen']['name'],
+                        version='1.0',
+                        url=session.mtrain['regimen']['script']
+                    ),
                 stimulus_modalities=[stim.OPTOGENETICS] if "opto" in epoch_name.lower() else [stim.VISUAL],
-                # stimulus_parameters=get_parameters(epoch_name),
+                # stimulus_parameters=,
                 # stimulus_device_names=get_device_names(epoch_name),
                 # speaker_config=get_speaker_config(epoch_name),
                 # reward_consumed_during_epoch=get_reward_consumed(epoch_name),
@@ -210,8 +252,8 @@ def generate_session_json(session_id: str) -> None:
         iacuc_protocol=iacuc_protocol(),
         rig_id=platform_json['rig_id'],
         subject_id=session.folder.split('_')[1],
-        data_streams=[ephys_stream(sync_path, recording_dirs, motor_locs_path, platform_json)],
-        stimulus_epochs=stim_epochs_from_table(stim_table_path, sync_path),
+        data_streams=data_streams(session.npexp_path, sync_path, recording_dirs, motor_locs_path, platform_json),
+        stimulus_epochs=stim_epochs_from_table(session, pkl_path, stim_table_path, sync_path),
         mouse_platform_name=mouse_platform_name(),
         active_mouse_platform=active_mouse_platform(),
         reward_consumed_unit='milliliter',
