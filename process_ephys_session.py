a1a3ebf7cbe4a93867a35bef3fe14483fbccb9e2
Carter Peene <carter.peene@alleninstitute.org>
2024-05-16 11:03:54 -0700

    add rig json and generation of all metadatas

--- process_ephys_session.py
+++ process_ephys_session.py
@@ -1,29 +1,19 @@
-import aind_data_schema
-import aind_data_schema.core.session as session_schema
-import aind_metadata_mapper
 import argparse
 import datetime
-import io
 import json
-import npc_ephys
-import npc_mvr
+import np_codeocean
 import np_session
-import npc_session
-import npc_sessions
-import npc_sync
-import numpy as np
 import pandas as pd
 import pathlib as pl
-from aind_data_schema.models.modalities import Modality as schema_modalities
-from aind_data_schema.models.coordinates import Coordinates3d as schema_coordinates
-from aind_metadata_mapper.ephys.camstim_session import CamstimSession
-from utils import process_ephys_sync as stim_utils
-from utils import pickle_functions as pkl_utils
 
 from aind_data_schema.core.data_description import Funding, DataDescription
-from aind_data_schema.models.organizations import Organization
-from aind_data_schema.models.pid_names import PIDName
-from aind_data_schema.models.platforms import Platform
+from aind_data_schema_models.modalities import Modality as schema_modalities
+from aind_data_schema_models.organizations import Organization
+from aind_data_schema_models.pid_names import PIDName
+from aind_data_schema_models.platforms import Platform
+from aind_metadata_mapper.ephys.camstim_ephys_session import CamstimEphysSession
+
+from utils import process_ephys_sync as stim_utils
 
 
 # defaults
@@ -103,10 +93,34 @@ modality_map = {
 }
 
 
-def generate_data_description_json(project_name: str, session: np_session.Session) -> None:
+def generate_rig_json(session: np_session.Session, overwrite: bool = False):
+    if (session.npexp_path / 'rig.json').exists():
+        print('rig.json already exists')
+        if not overwrite:
+            return
+        print('overwriting')
+
+    platform_path = next(session.npexp_path.glob(f'{session.folder}_platform*.json'))
+    platform_json = json.loads(platform_path.read_text())
+    rig_id = platform_json['rig_id']
+
+    rig_json_template = json.loads((pl.Path(__file__).parent / 'openscope_rig.json').read_text())
+    rig_json_template['rig_id'] = rig_id
+    rig_json_template['modification_date'] = str(datetime.date.today())
+
+    with open(session.npexp_path / 'rig.json', 'w', encoding='utf-8') as f:
+        json.dump(rig_json_template, f, ensure_ascii=False, indent=4)
+
+
+def generate_data_description_json(project_name: str, session: np_session.Session, overwrite: bool = False) -> None:
+    if (session.npexp_path / 'data_description.json').exists():
+        print('data_description.json already exists')
+        if not overwrite:
+            return
+        print('overwriting')
+    
     subject_id = session.folder.split('_')[1]
-    projects_info = pd.read_csv('./projects_info.csv', index_col='project_name')
-    print(projects_info)
+    projects_info = pd.read_csv(pl.Path(__file__).parent / 'projects_info.csv', index_col='project_name')
     project_info = projects_info.loc[project_name]
 
     funding_schemas = [Funding(funder=Organization.AI)]
@@ -130,7 +144,7 @@ def generate_data_description_json(project_name: str, session: np_session.Sessio
     data_description.write_standard_file(session.npexp_path)
 
 
-def generate_session_json(session_id: str, session: np_session.Session) -> str:    
+def generate_session_json(session_id: str, session: np_session.Session, overwrite: bool = False) -> str:    
     sync_path = session.npexp_path / f'{session.folder}.sync' 
     pkl_path = session.npexp_path / f'{session.folder}.stim.pkl'
     stim_table_path = session.npexp_path / f'{session.folder}_stim_epochs.csv' 
@@ -141,7 +155,7 @@ def generate_session_json(session_id: str, session: np_session.Session) -> str:
     platform_json = json.loads(platform_path.read_text())
     project_name = platform_json['project']
     experiment_info = json.loads(pl.Path(__file__).with_name('experiment_info.json').read_text())
-    
+
     if not stim_table_path.exists():
         print('building stim table')
         stim_utils.build_stimulus_table(pkl_path, sync_path, stim_table_path)
@@ -150,23 +164,37 @@ def generate_session_json(session_id: str, session: np_session.Session) -> str:
         opto_conditions = experiment_info[project_name].get('opto_conditions', DEFAULT_OPTO_CONDITIONS)
         stim_utils.build_optogenetics_table(opto_pkl_path, sync_path, opto_conditions, opto_table_path)
 
+    if (session.npexp_path / 'session.json').exists():
+        print('session.json already exists')
+        if not overwrite:
+            return project_name
+        print('overwriting')
+
     session_settings = experiment_info[project_name]
-    session_mapper = CamstimSession(session_id, session_settings)
+    session_mapper = CamstimEphysSession(session_id, session_settings)
     session_mapper.generate_session_json()
     session_mapper.write_session_json()
 
     return project_name
 
 
-def generate_jsons(session_id: str) -> None:
-    session = np_session.Session(session_id)
-    project_name = generate_session_json(session_id, session)
-    generate_data_description_json(project_name, session)
+def generate_jsons(session_ids: str, force: bool = False, no_upload: bool = False, overwrite: bool = False) -> None:
+    for session_id in session_ids:
+        print(f'\ngenerating jsons for session {session_id}')
+        session = np_session.Session(session_id)
+        project_name = generate_session_json(session_id, session, overwrite)
+        generate_data_description_json(project_name, session, overwrite)
+        generate_rig_json(session, overwrite)
+        if not no_upload:
+            np_codeocean.upload_session(session_id, force=force)
 
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description='Generate a session.json file for an ephys session')
-    parser.add_argument('session_id', help='session ID (lims or np-exp foldername) or path to session folder')
+    parser.add_argument('session_ids', nargs='+', help='one or more session IDs (lims or np-exp foldername) or path to session folder')
+    parser.add_argument('--no_upload', action='store_true', help='Don\'t run an upload job, just generate metadata files on npexp')
+    parser.add_argument('--force', action='store_true', help="enable `force_cloud_sync` option, re-uploading and re-making raw asset even if data exists on S3")
+    parser.add_argument('--overwrite', action='store_true', help='overwrite metadata files that already exist')
     return parser.parse_args()
 
 
