3ec8efd09a58fe7ff0497a5341efebca43467305
rcpeene <carter.peene@alleninstitute.org>
2025-08-04 17:28:58 -0700

    make pip installable

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="openscope_upload",
+    version="0.1.0",
+    packages=find_packages(),
+    description="Tools for OpenScope data upload and processing",
+    author="Your Name",
+    install_requires=[],
+)
